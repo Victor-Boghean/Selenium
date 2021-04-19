d90d940fe5fdc01bbe1d1ebd3ccfe824c965b222
Victor <victor.boghean15@gmail.com>
2021-04-19 13:11:01 +0300

    Laborator 4 > Selenium

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Protractor;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace TestareSoft
+{
+    class Program
+    {
+        private static IWebDriver driver;
+
+        
+        static void Main(string[] args)
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("--disable-notifications");
+            driver = new ChromeDriver(options);
+
+            
+            siteSearch("https://www.google.com/", new Xpaths().googleXpath, new Xpaths().googleHeader);
+            //siteSearch("https://www.youtube.com/", new Xpaths().youtubeXpath, new Xpaths().youtubeHeader);
+            //siteSearch("https://www.amazon.com/", new Xpaths().amazonXpath, new Xpaths().amazonHeader);
+            //siteSearch("https://www.ebay.com/", new Xpaths().ebayXpath, new Xpaths().ebayHeader);
+            //siteSearch("https://www.aliexpress.com/", new Xpaths().aliexpressXpath, new Xpaths().aliexpressHeader);
+            //siteSearch("https://999.md/ru/", new Xpaths().md999Xpath, new Xpaths().md999Header);
+            //siteSearch("https://www.reddit.com/", new Xpaths().redditXpath, new Xpaths().redditHeader);
+            //siteSearch("https://9gag.com/", new Xpaths().gang9Xpath, new Xpaths().gang9Header);
+
+        }
+        
+        //google test
+        static void siteSearch(string siteUrl, string searchXpath, string headerXpath)
+        {
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(siteUrl);
+            if (siteUrl == "https://9gag.com/")
+            {
+                driver.FindElement(By.XPath("//a[@class='search']")).Click();
+            }
+            var search = driver.FindElement(By.XPath(searchXpath));
+            search.Click();
+            search.SendKeys("computer");
+            search.SendKeys(Keys.Enter);
+
+            if (driver.FindElement(By.XPath(headerXpath)) != null)
+            {
+                Console.WriteLine("Header is displayed");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Header is not displayed");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Xpaths.cs
@@ -0,0 +1,38 @@
+﻿namespace TestareSoft
+{
+    public class Xpaths
+    {
+        //google
+        public string googleXpath = "//input[@name='q']";
+        public string googleHeader = "//a[@id='logo']";
+        
+        
+        //youtube
+        public string youtubeXpath = "//input[@name='search_query']";
+        public string youtubeHeader = "//yt-icon[@id='logo-icon']";
+        
+        //amazon
+        public string amazonXpath = "//input[@name='field-keywords']";
+        public string amazonHeader = "//a[@id='nav-logo-sprites']";
+        
+        //ebay
+        public string ebayXpath = "//input[@name='_nkw']";
+        public string ebayHeader = "//a[@id='gh-la']";
+
+        //aliexpress
+        public string aliexpressXpath = "//input[@name='SearchText']";
+        public string aliexpressHeader = "//span[@class='logo-base']";
+
+        //999.md
+        public string md999Xpath = "//input[@name='query']";
+        public string md999Header = "//h1[@class='header_bar_logo']";
+        
+        //reddit
+        public string redditXpath = "//input[@type='search']";
+        public string redditHeader = "//a[@class='_30BbATRhFv3V83DHNDjJAO']";
+
+        //9gang
+        public string gang9Xpath = "//input[@name='query']";
+        public string gang9Header = "//div[@class='nav-wrap']";
+    }
+}
